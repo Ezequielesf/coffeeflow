7a682957d0f18d5edbdefb9a0d30f3efec573723
Ezequielesf <155244937+Ezequielesf@users.noreply.github.com>
2026-09-03 18:01:13 -0300

    Update cakto-webhook.js

--- api/cakto-webhook.js
+++ api/cakto-webhook.js
@@ -126,7 +126,8 @@ export default async function handler(req, res) {
     // EVENTO IGNORADO
     // ==========================================
 
-    return res.status(200).json({
+        return res.status(200).json({ success: true });
+
       ok: true,
       ignorado: evento
     });
